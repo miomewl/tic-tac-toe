e2f9fea6c30e7512780c04e1741c2e5dc421a323
miomewl <miller.lero4ka@gmail.com>
2026-01-18 22:36:50 +0100

    Setup herní plochy

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -7,3 +7,21 @@ discord: idk
 
 """
 
+def main():
+    print("Vítejte ve hře Tic Tac Toe")
+    
+    # Vytvoření prázdné plochy (seznam 9 mezer)
+    herni_plocha = [' ' for _ in range(9)]
+    hra_bezi = True
+
+    while hra_bezi:
+        # Jednoduchý výpis plochy
+        print(herni_plocha[0:3])
+        print(herni_plocha[3:6])
+        print(herni_plocha[6:9])
+        
+        # Aby se smyčka nezasekla, zatím ji ukončíme hned
+        hra_bezi = False
+
+if __name__ == "__main__":
+    main()
